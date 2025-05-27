026b685d2d2731c61ac4d8ed7239878028ab4255
Eric M Kerney <95364170+eKerney@users.noreply.github.com>
2025-05-27 12:57:14 -0400

    updated renderlayers

--- src/RenderLayers.tsx
+++ src/RenderLayers.tsx
@@ -26,40 +26,44 @@ interface ArcGISFeatureLayer {
 }
 
 type Layer = GroupLayer | ArcGISFeatureLayer;
-type NestedArrayItem = Layer | any[] | number | null;
+type NestedArrayItem = Array<Layer> | Layer;
 
 export const RenderLayers = ({ nestedLayers }: { nestedLayers: NestedArrayItem }) => {
 
   function layerRenderer(layers0: NestedArrayItem): React.JSX.Element {
     switch (true) {
-      case Array.isArray(layers0):
+      case layers0 !== null
+        && Array.isArray(layers0):
         console.log('Array', layers0)
-        return layers0.map((layers1: NestedArrayItem) => (
-          <div key={layers1.id} className="group">
-            <h4>{layers1.title} - {layers1.id}</h4>
-            <RenderLayers nestedLayers={layers1} />
-          </div>
-        ))
+        return (
+          <>
+            {layers0.map((layers1: NestedArrayItem, i) => (
+              <div key={`layerArray-${i}`} className="group">
+                <RenderLayers nestedLayers={layers1} />
+              </div>
+            ))}
+          </>);
 
       case typeof layers0 === 'object'
         && layers0 !== null
         && 'layerType' in layers0
         && layers0.layerType === LayerType.GROUP_LAYER:
         if (!('layers' in layers0)) return (<>Invalid GroupLayer: missing layers</>);
-        console.log('GroupLayer', layers0.title, layers0.layers, layers0.id)
-        return layers0.layers.map((layers1: GroupLayer | ArcGISFeatureLayer) => (
-          <div className="group-children">
-            <RenderLayers nestedLayers={layers1} />
+        console.log('GroupLayer', layers0.title)
+        return (
+          <div key={layers0.id} className="group-children">
+            <h3>{layers0.title} - {layers0.id}</h3>
+            {layers0.layers.map((layers1: GroupLayer | ArcGISFeatureLayer) => <RenderLayers nestedLayers={layers1} />)}
           </div>
-        ))
+        )
 
       case typeof layers0 === 'object'
         && layers0 !== null
         && 'layerType' in layers0
         && layers0.layerType === LayerType.FEATURE_LAYER:
         if (!('url' in layers0)) return (<>Invalid ArcGISFeatureLayer: missing url</>);
-        console.log('ArcGISFeatureLayer', layers0.title, layers0.id)
-        return (<div key={layers0.id} className="layer"> {layers0.id} - {layers0.layerType} - {layers0.url}<br /></div>)
+        console.log('ArcGISFeatureLayer', layers0.title)
+        return (<div key={layers0.id} className="layer"> {layers0.id} - {layers0.title} - {layers0.layerType} <br /></div>)
       default:
         return <>`Other: ${typeof layers0}`</>;
     }
